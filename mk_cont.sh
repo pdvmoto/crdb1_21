2f4aa51ed9f421779f53f6d91561f1160cf36583
pdvmoto <piet.de.visser@gmail.com>
2026-03-04 07:09:11 +0100

    makecont

--- mk_cont.sh
+++ mk_cont.sh
@@ -6,10 +6,17 @@
 # note: -v mapping required dirs to be known by docker. Dont map if volumes not know..
 #     check also: mk_oc2.sh: more elaborate commands and logging
 #
+# note: when using local data-dir, it should  be in ~/oradata/$CONT, 
+#   that way, we use the hostname, $CONT, to designate a subdir in ORADATA for each container
+#
 # note: initdb and startdb contain various tricks.. if exit non-zero, container will FAIL
 #       may needd to create subdirs and put files in them
 #
 # note: when mapping oradata, database will (can) persist, but versions upgrades ??
+# 
+# note: the entrypoint for the free-containers.. override trick in separate notes.txt..
+# 
+# note: script works best with gvenzl-free, need testing for "enterprise" 
 #
 #
 # Tips: 
@@ -20,21 +27,34 @@
 # todo:
 # - oradata-mount or mapping: include container-name, to allow mulitple.
 # - better replacement of entrypoint-script ?  
+# - add rlwrap and aliases for sq
+#     microdnf install oracle-epel-release-el8 
+#     microdnf install rlwrap
+# - consider X11, test, keep script/notes, but do not integraate into script
 #
 
 
-# HOSTNAME and containername..
+# define HOSTNAME and containername..
 
-CONT=o26fa
+CONT=o26fs
+
+# define the program used for yum / dnf / microdnf (gvenzl)
+# gvenzl: microdnf
+# enterprise: yum or dnf
+
+YUM=microdnf 
 
 # IMAGE to use..
 
-# SRC_IMAGE=gvenzl/oracle-free:slim
+  SRC_IMAGE=gvenzl/oracle-free:slim
 # SRC_IMAGE=gvenzl/oracle-free:full-faststart
-  SRC_IMAGE=gvenzl/oracle-free:full
+# SRC_IMAGE=gvenzl/oracle-free:full
+# SRC_IMAGE=container-registry.oracle.com/database/enterprise
+
 
 
 # create+prepare map-volumes
+# by default, we map to subdirs of where the script is, change at will
 mkdir ./map_initdb
 mkdir ./map_startdb
 mkdir ./map_diag
@@ -49,11 +69,12 @@ cp bash_profile_extra   ./map_initdb/bash_profile_extra
 
 docker run -d  \
   --hostname $CONT \
-  --name     $CONT -p1521:1521 \
+  --name     $CONT \
+  -p1521:1521 \
   -v        ./map_startdb:/container-entrypoint-startdb.d \
   -v         ./map_initdb:/container-entrypoint-initdb.d  \
   -v           ./map_diag:/opt/oracle/diag  \
-  -v /Users/pdvbv/oradata:/opt/oracle/oradata  \
+  -v /Users/pdvbv/oradata/$CONT:/opt/oracle/oradata  \
   -e ORACLE_PASSWORD=oracle   \
   $SRC_IMAGE 
 
@@ -75,16 +96,16 @@ docker run -d  \
 #
 # further config..
 #
-docker exec -u root $CONT microdnf install sysstat
-docker exec -u root $CONT microdnf install which
-docker exec -u root $CONT microdnf install file
-docker exec -u root $CONT microdnf install procps
-docker exec -u root $CONT microdnf install git
+docker exec -u root $CONT $YUM install sysstat
+docker exec -u root $CONT $YUM install which
+docker exec -u root $CONT $YUM install file
+docker exec -u root $CONT $YUM install procps
+docker exec -u root $CONT $YUM install git
 
 echo "dont forget git-clone... " 
-docker exec         $CONT git clone https://github.com/pdvmoto/binsql   /opt/oracle/admin/binsql
-docker exec         $CONT git clone https://github.com/pdvmoto/crdb1_21 /opt/oracle/admin/crdb1_21
-docker exec         $CONT rm                                            /opt/oracle/admin/crdb1_21/*.png
+docker exec  $CONT  git clone https://github.com/pdvmoto/binsql   /opt/oracle/admin/binsql
+docker exec  $CONT  git clone https://github.com/pdvmoto/crdb1_21 /opt/oracle/admin/crdb1_21
+docker exec  $CONT  rm                                            /opt/oracle/admin/crdb1_21/*.png
 
 echo .
 docker ps 
